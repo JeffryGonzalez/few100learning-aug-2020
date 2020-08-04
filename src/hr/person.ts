d99332be88dbb257ed055ffe6760a418a03ca16d
Jeffry Gonzalez <jeff@hypertheory.com>
2020-08-04 16:58:56 -0400

    done with tuesday

---
+++ src/hr/person.ts
@@ -0,0 +1,7 @@
+export abstract class Person {
+
+    public firstName: string;
+    public lastName: string;
+
+    abstract getInfo(): string;
+}
---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+
+
+export const isEven = (n: number): boolean => n % 2 === 0;
+
+export const isOdd = (n: number): boolean => !isEven(n);
+
+export interface Thingy {
+
+}
+
+class Monkey {
+
+}
+
+export type MathThingy = number;
+
+export type PI = 3.1514927;
